57b9b6763f61097af31368eae2a6585ed051de5c
jamesdotcom <james@appirio.com>
2015-01-14 13:36:52 -0500

    updated to ref repos in git

--- checkout-all.sh
+++ checkout-all.sh
@@ -4,39 +4,27 @@ BRANCH=$1
 
 if [ -z "$BRANCH" ]
 then
-  echo 'usage: checkout-all.sh <git branch> <svn branch>'
+  echo 'usage: checkout-all.sh <git branch>'
   exit 1
 fi
 
-SVN_BRANCH=$2
-if [ -z "$SVN_BRANCH" ]
-then
-  echo 'usage: checkout-all.sh <git branch> <svn branch>'
-  exit 2
-fi
-
-echo Checking out branch $SVN_BRANCH
-
 cd ~/dev
-svn co https://coder.topcoder.com/internal/glue/$SVN_BRANCH glue
-svn co https://coder.topcoder.com/internal/libs/encoder/$SVN_BRANCH libs/encoder
-svn co https://coder.topcoder.com/internal/libs/logging/$SVN_BRANCH libs/logging
-svn co https://coder.topcoder.com/internal/libs/client-socket/$SVN_BRANCH libs/client-socket
-svn co https://coder.topcoder.com/internal/libs/nbio-listener/$SVN_BRANCH libs/nbio-listener
-svn co https://coder.topcoder.com/internal/libs/custom-serialization/$SVN_BRANCH libs/custom-serialization
-svn co https://coder.topcoder.com/internal/libs/concurrent/$SVN_BRANCH libs/concurrent
-svn co https://coder.topcoder.com/internal/libs/http-tunnel/server/$SVN_BRANCH libs/http-tunnel/server
-svn co https://coder.topcoder.com/internal/libs/http-tunnel/client/$SVN_BRANCH libs/http-tunnel/client
-svn co https://coder.topcoder.com/internal/farm-client/$SVN_BRANCH farm-client
-svn co https://coder.topcoder.com/internal/farm-deployer/$SVN_BRANCH farm-deployer
-svn co https://coder.topcoder.com/internal/farm-shared/$SVN_BRANCH farm-shared
-svn co https://coder.topcoder.com/internal/shared/$SVN_BRANCH shared
-svn co https://coder.topcoder.com/internal/comp-eng/client-common/$SVN_BRANCH comp-eng/client-common
-
 echo Checking out branch $BRANCH
-
-# unique git hostnames are needed for multiple github deploy keys - see ~/.ssh/config
-git clone git@git-farm-server:appirio-tech/arena-farm-server.git -b $BRANCH farm-server
+git clone git@github.com:appirio-tech/arena-glue.git -b $BRANCH glue
+git clone git@github.com:appirio-tech/arena-encoder.git -b $BRANCH libs/encoder
+git clone git@github.com:appirio-tech/arena-logging.git -b $BRANCH libs/logging
+git clone git@github.com:appirio-tech/arena-client-socket.git -b $BRANCH libs/client-socket
+git clone git@github.com:appirio-tech/arena-nbio-listener.git -b $BRANCH libs/nbio-listener
+git clone git@github.com:appirio-tech/arena-custom-serialization.git -b $BRANCH libs/custom-serialization
+git clone git@github.com:appirio-tech/arena-concurrent.git -b $BRANCH libs/concurrent
+git clone git@github.com:appirio-tech/arena-http-tunnel-server.git -b $BRANCH libs/http-tunnel/server
+git clone git@github.com:appirio-tech/arena-http-tunnel-client.git -b $BRANCH libs/http-tunnel/client
+git clone git@github.com:appirio-tech/arena-farm-client.git -b $BRANCH farm-client
+git clone git@github.com:appirio-tech/arena-farm-deployer.git -b $BRANCH farm-deployer
+git clone git@github.com:appirio-tech/arena-farm-shared.git -b $BRANCH farm-shared
+git clone git@github.com:appirio-tech/arena-shared.git -b $BRANCH shared
+git clone git@github.com:appirio-tech/arena-comp-eng-client-common.git -b $BRANCH comp-eng/client-common
+git clone git@github.com:appirio-tech/arena-farm-server.git -b $BRANCH farm-server
 git clone git@github.com:cloudspokes/arena-shared.git -b $BRANCH comp-eng/arena-shared
 git clone git@github.com:cloudspokes/arena-client.git -b $BRANCH comp-eng/arena-client
 git clone git@github.com:cloudspokes/mpsqas-client.git -b $BRANCH comp-eng/mpsqas-client
