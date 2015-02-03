5f228cd8ce3724404c7659eea4392789480aa462
Guiqiang Zhang <skyhit@gmail.com>
2015-02-03 17:15:46 +0800

    update vm scripts

---
+++ start-farm-processor.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+rm -rf ~/processor/work
+mkdir ~/processor/work
+rm -rf ~/processor/cache
+mkdir ~/processor/cache
+chmod +x ~/processor/deploy/app/cpp/timeout/timeout
+
+
+OS_BITS=`getconf LONG_BIT`
+CPP_ARGUMENTS="-DcppArguments=--std=c++0x"
+if [ $OS_BITS -eq 32 ]; then
+    CPP_ARGUMENTS=""
+fi
+
+# sqs.topcoder.com:9324 = elasticmq (remote sqs server)
+export JAVA_OPTS="$CPP_ARGUMENTS -Darena.sqs-endpoint=http://sqs.topcoder.com:9324 -Darena.env-prefix=dev -Daws.accessKeyId=x -Daws.secretKey=x"
+
+cd ~/processor/deploy/bin
+echo Starting processor...
+./processor.sh PR-LX
+
+echo Startup complete
+exit
--- start-services_arena.sh
+++ start-services_arena.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
-rm -rf ~/processor/work
-mkdir ~/processor/work
-rm -rf ~/processor/cache
-mkdir ~/processor/cache
-chmod +x ~/processor/deploy/app/cpp/timeout/timeout
-
 echo Elasticmq Server...
 sh ~/dev/arena-vm/start-sqs-service.sh
 
 sleep 2
 
-OS_BITS=`getconf LONG_BIT`
-CPP_ARGUMENTS="-DcppArguments='--std=c++0x'"
-if [ $OS_BITS -eq 32 ]; then
-    CPP_ARGUMENTS=""
-fi
-
 # localhost:9324 = elasticmq (local sqs)
 # also set dummy aws credentials
-export JAVA_OPTS="$CPP_ARGUMENTS -Darena.sqs-endpoint=http://localhost:9324 -Darena.env-prefix=dev -Daws.accessKeyId=x -Daws.secretKey=x -DconfigurationProvider.class=com.topcoder.farm.controller.configuration.XMLConfigurationProvider -Dconfiguration.xml.url=file:///home/apps/app/controller/config.xml"
+export JAVA_OPTS="-Darena.sqs-endpoint=http://localhost:9324 -Darena.env-prefix=dev -Daws.accessKeyId=x -Daws.secretKey=x -DconfigurationProvider.class=com.topcoder.farm.controller.configuration.XMLConfigurationProvider -Dconfiguration.xml.url=file:///home/apps/app/controller/config.xml"
 export JBOSS_JAVA_OPTS="$JBOSS_JAVA_OPTS -Djavax.net.ssl.trustStore=TC.cloud.ldap.keystore"
 
 cd ~/app/scripts
@@ -45,9 +33,7 @@ echo Starting mpsqas listener...
 echo Starting web socket listener...
 ./runWebSocketListener.sh
 
-cd ~/processor/deploy/bin
-echo Starting processor...
-./processor.sh PR-LX
+sh ~/dev/arena-vm/start-farm-processor.sh
 
 echo Startup complete
 exit
