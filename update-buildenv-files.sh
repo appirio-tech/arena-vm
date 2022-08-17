e5163bcd60ed3b9010513c1a12c085b031184739
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2022-08-17 11:40:46 +0530

    Arena deployment

---
+++ update-buildenv-files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mv -rf *.properties.$LOGICAL_ENV repos/app/
+
+mv -f TC.cloud.ldap.keystore repos/app/scripts/
+mv -f LDAP.properties repos/app/resources/
+
+mv -f security.keystore.cloud repos/app/scripts/
+mv -f Util.properties repos/app/resources/com/topcoder/security/
