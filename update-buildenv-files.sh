49846ad412490d0617b8736a9089f8d6a0758ecd
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2022-08-17 21:21:27 +0530

    Arena deployment

--- update-buildenv-files.sh
+++ update-buildenv-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mv -f token.properties.$LOGICAL_ENV repos/app/
 
-mv -f TC.cloud.ldap.keystore repos/app/scripts/
+mv -f TC.prod.ldap.keystore repos/app/scripts/TC.cloud.ldap.keystore
 mv -f LDAP.properties repos/app/resources/
 
 mv -f security.keystore.cloud repos/app/scripts/
