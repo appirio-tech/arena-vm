f7db6a1db58708f508b8e4d2d46b4a288e27cff2
liuliquan <leviastan@gmail.com>
2022-08-22 23:58:23 +0800

    enable tls1.0 for jboss

--- repos/app/scripts/jboss.sh
+++ repos/app/scripts/jboss.sh
@@ -8,12 +8,18 @@ if [ "$JAVA_VER" -ge 55 ] ; then
   ENDORSED_DIR=""
 fi
 
+CUSTOM_SECURITY=""
+if [ "$JAVA_VER" -ge 52 ] ; then
+  echo "Use custom.security for JDK >= 1.8"
+  CUSTOM_SECURITY="-Djava.security.properties=custom.security"
+fi
+
 if [[ -z "${JBOSS_JAVA_OPTS}" ]] ; then
 	JBOSS_JAVA_OPTS="-Xms2048m -Xmx8192m"
 fi
 JBOSS_JAVA_OPTS="$JBOSS_JAVA_OPTS -Djavax.net.ssl.trustStore=TC.cloud.ldap.keystore"
 
 echo "nohup java ${JBOSS_JAVA_OPTS} $ENDORSED_DIR -classpath $JBOSS_HOME/bin/run.jar:$CP org.jboss.Main &"
-nohup java ${JBOSS_JAVA_OPTS} $ENDORSED_DIR -classpath $JBOSS_HOME/bin/run.jar:$CP org.jboss.Main &
+nohup java ${JBOSS_JAVA_OPTS} $CUSTOM_SECURITY $ENDORSED_DIR -classpath $JBOSS_HOME/bin/run.jar:$CP org.jboss.Main &
 
 exit
